f07aecc9988dfff0b5f84fe3e792cd7a10fbc029
adam-hindle <adam.hindle@digital.hmrc.gov.uk>
2019-11-14 16:11:07 +0000

    Departures overview journey

--- app/routes.js
+++ app/routes.js
@@ -3,8 +3,4 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-//Routes for Departures
-
-router.use('/departures/version-1', require('./views/departures/version-1/routes'));
-
 module.exports = router
