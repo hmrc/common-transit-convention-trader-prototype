56adccf58e2d2f87cca5571db7d8b84a200e40be
Ladine Cook <Ladine.cook@digital.hmrc.gov.uk>
2020-01-13 15:55:27 +0000

    removed service name and removed back link for arrival sent

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Common Transit Convention for traders',
+  serviceName: '',
 
   // Default port that prototype runs on
   port: '3000',
