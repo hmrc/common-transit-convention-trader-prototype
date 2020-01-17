d822e37a1a54fa98d45e7509d2f7455c6c57c213
adam-hindle <adam.hindle@digital.hmrc.gov.uk>
2020-01-17 16:54:40 +0000

    In progress improvements from gathered feedback

--- app/views/unloading-remarks/main-seals/routes.js
+++ app/views/unloading-remarks/main-seals/routes.js
@@ -2,6 +2,24 @@ const express = require('express')
 const router = express.Router()
 
 /*
+    If the user selects "Yes" to "Do you need to change the vehicle name, registration or reference?", then they are taken to "new-id-at-departure"
+
+    If the user selects "No", then they are taken to "change-transport-nationality-at-departure"
+*/
+
+router.post('/change-id-at-departure', function (req, res) {
+
+    let changeIdAtDeparture = req.session.data.changeIdAtDeparture;
+
+    if ( changeIdAtDeparture == ('Yes')) {
+        res.redirect('new-id-at-departure');
+    } else {
+        res.redirect('change-transport-nationality-at-departure');
+    }
+
+})
+
+/*
     If the user selects "yes" to "unloading complete", then they are taken to "check unloading permission"
 
     Anything else and the user is taken to "change-id-at-departure"
