2d954c5acd73e917e245678f73cd81a3b65ec7f2
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2021-11-16 17:13:11 +0000

    Placeholder content for x914 access code.

--- app/views/guarantee-balance/routes.js
+++ app/views/guarantee-balance/routes.js
@@ -3,26 +3,28 @@ const session = require('express-session');
 const router = express.Router()
 
 router.post('/eori', function (req, res) {
-    res.redirect("guarantee-reference")
+    res.redirect("guarantee-reference");
 });
 
 router.post('/guarantee-reference', function (req, res) {
-    res.redirect("access-code")
+    res.redirect("access-code");
 });
 
 router.post('/access-code', function (req, res) {
-    res.redirect("check-your-answers")
+    res.redirect("check-your-answers");
 });
 
 router.post('/check-your-answers', function (req, res) {
-    let accessCode = req.session.data.accessCode
+    let accessCode = req.session.data.accessCode;
 
     if (accessCode.endsWith("906")) {
-        res.redirect("details-do-not-match")
+        res.redirect("details-do-not-match");
     } else if (accessCode.endsWith("000")) {
-        res.redirect("please-wait")
+        res.redirect("please-wait");
+    } else if (accessCode.endsWith("914")) {
+        res.redirect("guarantee-type-not-supported");
     } else {
-        res.redirect("balance-confirmation")
+        res.redirect("balance-confirmation");
     }
 });
 
